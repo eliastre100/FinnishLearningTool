c1c456749978978c88ca47dafcea17a18624f03a
Antoine FORET <antoine.foret@epitech.eu>
2018-11-04 20:01:31 +0200

    [~] Improved month page

--- src/app/months/months.component.ts
+++ src/app/months/months.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 declare var M: any;
 
-interface QuestionType { origin: string, answer: string }
+interface QuestionType { origin: string, answer: string, enabled: boolean }
 @Component({
   selector: 'app-months',
   templateUrl: './months.component.html',
@@ -10,18 +10,18 @@ interface QuestionType { origin: string, answer: string }
 })
 export class MonthsComponent implements OnInit {
   private questions: QuestionType[] = [
-    { origin: 'January', answer: 'Tammikuu' },
-    { origin: 'February', answer: 'Helmikuu' },
-    { origin: 'March', answer: 'Maaliskuu' },
-    { origin: 'April', answer: 'Huhtikuu' },
-    { origin: 'May', answer: 'Toukokuu' },
-    { origin: 'June', answer: 'Kesäkuu' },
-    { origin: 'July', answer: 'Heinäkuu' },
-    { origin: 'August', answer: 'Elokuu' },
-    { origin: 'September', answer: 'Syyskuu' },
-    { origin: 'October', answer: 'Lokakuu' },
-    { origin: 'November', answer: 'Marraskuu' },
-    { origin: 'December', answer: 'Joulukuu' },
+    { origin: 'January', answer: 'Tammikuu', enabled: true },
+    { origin: 'February', answer: 'Helmikuu', enabled: true },
+    { origin: 'March', answer: 'Maaliskuu', enabled: true },
+    { origin: 'April', answer: 'Huhtikuu', enabled: true },
+    { origin: 'May', answer: 'Toukokuu', enabled: true },
+    { origin: 'June', answer: 'Kesäkuu', enabled: true },
+    { origin: 'July', answer: 'Heinäkuu', enabled: true },
+    { origin: 'August', answer: 'Elokuu', enabled: true },
+    { origin: 'September', answer: 'Syyskuu', enabled: true },
+    { origin: 'October', answer: 'Lokakuu', enabled: true },
+    { origin: 'November', answer: 'Marraskuu', enabled: true },
+    { origin: 'December', answer: 'Joulukuu', enabled: true },
   ];
   public question = null;
   public show = false;
@@ -35,16 +35,20 @@ export class MonthsComponent implements OnInit {
 
   selectQuestion() {
     let q = null;
+    const enabledQuestions = this.questions.filter((element) => element.enabled);
+    if (enabledQuestions.length < 1) {
+      alert('No question enabled. Please enable more question to use the quizz feature');
+      return;
+    }
     do {
-      q = this.questions[Math.floor(Math.random() * 10) % this.questions.length];
-    } while (q === this.question);
+      q = enabledQuestions[Math.floor(Math.random() * 100) % enabledQuestions.length];
+    } while (q === this.question && enabledQuestions.length > 1);
     this.question = q;
     this.show = false;
     this.answer = '';
   }
 
   checkAnswer() {
-    console.log(this.answer, this.question.answer);
     if (this.answer.toLowerCase() === this.question.answer.toLowerCase()) {
       M.toast({html: 'Good answer', classes: 'green'});
       this.selectQuestion();
@@ -54,7 +58,7 @@ export class MonthsComponent implements OnInit {
   }
 
   reverse() {
-    const qs = this.questions.map((question) => ({ origin: question.answer, answer: question.origin }));
+    const qs = this.questions.map((question) => ({ origin: question.answer, answer: question.origin, enabled: question.enabled }));
     this.questions = qs;
   }
 }
